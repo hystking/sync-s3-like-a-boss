5c6185a5f639dbaf983fcb1a4b35dca0d3a10a14
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2016-01-13 17:57:08 +0900

    add comments

--- invalidation.sh
+++ invalidation.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 set -e
+
 SCRIPT_DIR=`dirname $0`
 TIMESTAMP=`date +%s`
 
+#-----------------+
+#     CONFIGS     |
+#-----------------+
+
+# aws-cli profile
 AWS_PROFILE=my-aws-profile
+
+# CloudFront Distribution ID
 CLOUDFRONT_DISTRIBUTION_ID=MY_CLOUDFRONT_DISTRIBUTION_ID
+
+# template to use new invalidation.json
 INVALIDATION_TEMPLATE_JSON=$SCRIPT_DIR/templates/invalidation.json
+
+# temporary file path
 INVALIDATION_JSON=$SCRIPT_DIR/invalidation-temp.json
+
+# editor command to edit  invalidation.json
 EDITOR=vim
+
+#-----------------+
+#    PROCEDURE    |
+#-----------------+
 
 CREATE_INVALIDATION_COMMAND="aws cloudfront create-invalidation --profile $AWS_PROFILE"
 
--- sync.sh
+++ sync.sh
@@ -2,9 +2,22 @@
 set -e
 SCRIPT_DIR=`dirname $0`
 
+#-----------------+
+#     CONFIGS     |
+#-----------------+
+
+# aws-cli profile
 AWS_PROFILE=my-aws-profile
+
+# root path to sync
 LOCAL_PATH=$SCRIPT_DIR/public/
+
+# S3 bucket URI
 S3_URI=s3://my-buket
+
+#-----------------+
+#    PROCEDURE    |
+#-----------------+
 
 SYNC_COMMAND="aws s3 sync $LOCAL_PATH $S3_URI --delete --cache-control no-cache --profile $AWS_PROFILE"
 
